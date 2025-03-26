58fd64d6db0864504e7444364e3d42d89259015e
Jenie <jdft1101@gmail.com>
2025-03-26 15:22:24 -0300

    Adicionar arquivos de projeto.

---
+++ Gerenciador de Tarefas/Classes.cs
@@ -0,0 +1,24 @@
+﻿namespace Gerenciador_de_Tarefas
+{
+    internal class NovoResponsavel
+    {
+        public string nome;
+        public string email;
+
+        public NovoResponsavel(string nome, string email)
+        {
+            this.nome = nome;
+            this.email = email;
+        }
+    }
+
+    class NovaTarefa
+    {
+        public string titulo;
+        //public -- dataLimite;
+        //public -- prioridade;
+        //public -- responsavel;
+
+
+    }
+}
---
+++ Gerenciador de Tarefas/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace Gerenciador_de_Tarefas
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine($@"
+                 Digite o número da opção que deseja:
+                 1 - Cadastrar um novo usuário.
+                 2 - Cadastrar uma nova tarefa.
+                 3 - Excluir uma tarefa.
+                 4 - Atualizar o status de uma tarefa.
+                 5 - Listar tarefas.
+                 0 - Sair.
+                ");
+
+                // fazer o tratamento de erros
+
+                string opcao = Console.ReadLine();
+
+                try
+                {
+                    switch (opcao)
+                    {
+                        case "0":
+                            Console.WriteLine("Saindo...");
+                            return;
+                        case "1":
+                            Console.WriteLine("Cadastrar um novo usuário");
+                            break;
+                        case "2":
+                            Console.WriteLine("Cadastrar uma nova tarefa");
+                            break;
+                        case "3":
+                            Console.WriteLine("Excluir uma tarefa.");
+                            break;
+                        case "4":
+                            Console.WriteLine("Atualizar o status de uma tarefa");
+                            break;
+                        case "5":
+                            Console.WriteLine("Listar tarefas.");
+                            break;
+                        default: 
+                            opcao = "Inválido";
+                            Console.WriteLine("Escolha uma opção de 0 até 5:");
+                            break;
+                    }
+                } catch (Exception ex) {
+                    Console.WriteLine($"Exceção 1 - Exception\n{ex}");
+                }
+            }
+        }
+
+    }
+}
